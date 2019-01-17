9547f1f731667b6e162349a9305f04647cb57860
shaimayshah <shaimay.pathways@gmail.com>
2019-01-17 22:22:26 +0900

    Bug fixes

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,29 +1,27 @@
 $(document).ready(function() {
     $(".red").click(function() {
         $("h2, .lol, .options").css("visibility", "hidden");
-        $("td").css("visibility", "visible");
-        aiCo = "blue";
-        huCo = "red";
-        console.log("Human chooses red!");
+        $("table").css("visibility", "visible");
+        ai = "blue";
+        human = "red";
     });
     $(".blue").click(function() {
         $("h2, .lol, .options").css("visibility", "hidden");
-        $("td").css("visibility", "visible");
-        console.log("Human chooses blue!");
+        $("table").css("visibility", "visible");
     });
 
     $("td").click(function() {
-        move(this, huPlayer, huCo);
-        console.log("clicked");
+        move(this, hu, human);
     });
 });
 var board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-var huPlayer = "P";
-var aiPlayer = "C";
-var iter = 0;
+var hu = "H";
+var a = "A";
+var count = 0;
 var round = 0;
-var aiCo = "red";
-var huCo = "blue";
+var ai = "red";
+var human = "blue";
+
 
     function reset() {
         round = 0;
@@ -31,112 +29,6 @@ var huCo = "blue";
         $("td").css("background-color", "transparent");
     }
 
-    function move(element, player, color) {
-        console.log("element"+ element.id);
-        if (board[element.id] != "H" && board[element.id] != "C") {
-            round++;
-            $(element).css("background-color", color);
-            board[element.id] = player;
-            console.log(board);
-
-            if (winning(board, player)) {
-                setTimeout(function() {
-                    alert("YOU WIN");
-                    reset();
-                }, 500);
-                return;
-            } else if (round > 8) {
-                setTimeout(function() {
-                    alert("TIE");
-                    reset();
-                }, 500);
-                return;
-            } else {
-                round++;
-                var index = minimax(board, aiPlayer).index;
-                var selector = "#" + index;
-                $(selector).css("background-color", aiCo);
-                board[index] = aiPlayer;
-                console.log(board);
-                console.log(index);
-                if (winning(board, aiPlayer)) {
-                    setTimeout(function() {
-                        alert("YOU LOSE");
-                        reset();
-                    }, 500);
-                    return;
-                } else if (round === 0) {
-                    setTimeout(function() {
-                        alert("tie");
-                        reset();
-                    }, 500);
-                    return;
-                }
-            }
-        }
-    }
-
-    function minimax(reboard, player) {
-        iter++;
-        let array = avail(reboard);
-        if (winning(reboard, huPlayer)) {
-            return {
-                score: -10
-            };
-        } else if (winning(reboard, aiPlayer)) {
-            return {
-                score: 10
-            };
-        } else if (array.length === 0) {
-            return {
-                score: 0
-            };
-        }
-
-        var moves = [];
-        for (var i = 0; i < array.length; i++) {
-            var move = {};
-            move.index = reboard[array[i]];
-            reboard[array[i]] = player;
-
-            if (player == aiPlayer) {
-                var g = minimax(reboard, huPlayer);
-                move.score = g.score;
-            } else {
-                var g = minimax(reboard, aiPlayer);
-                move.score = g.score;
-            }
-            reboard[array[i]] = move.index;
-            moves.push(move);
-        }
-
-        var bestMove;
-        if (player === aiPlayer) {
-            var bestScore = -10000;
-            for (var i = 0; i < moves.length; i++) {
-                if (moves[i].score > bestScore) {
-                    bestScore = moves[i].score;
-                    bestMove = i;
-                }
-            }
-        } else {
-            var bestScore = 10000;
-            for (var i = 0; i < moves.length; i++) {
-                if (moves[i].score < bestScore) {
-                    bestScore = moves[i].score;
-                    bestMove = i;
-                }
-            }
-        }
-        return moves[bestMove];
-    }
-
-//available spots
-    function avail(reboard) {
-        return reboard.filter(s => s != "P" && s != "C");
-    }
-
-// winning combinations
     function winning(board, player) {
         if (
             (board[0] == player && board[1] == player && board[2] == player) ||
@@ -153,3 +45,106 @@ var huCo = "blue";
             return false;
         }
     }
+
+    function move(element, player, color) {
+        if (board[element.id] != "H" && board[element.id] != "A") {
+            round++;
+            $(element).css("background-color", color);
+            board[element.id] = player;
+
+            if (winning(board, player)) {
+                setTimeout(function() {
+                    alert("YOU WIN");
+                    reset();
+                }, 500);
+                return;
+            } else if (round > 8) {
+                setTimeout(function() {
+                    alert("TIE");
+                    reset();
+                }, 500);
+                return;
+            } else {
+                round++;
+                var index = minimax(board, a).index;
+                var selector = "#" + index;
+                $(selector).css("background-color", ai);
+                board[index] = a;
+                if (winning(board, a)) {
+                    setTimeout(function() {
+                        alert("YOU LOSE");
+                        reset();
+                    }, 500);
+                    return;
+                } else if (round === 0) {
+                    setTimeout(function() {
+                        alert("tie");
+                        reset();
+                    }, 500);
+                    return;
+                }
+            }
+        }
+    }
+
+    function minimax(newBoard, player) {
+        count++;
+        let array = avail(newBoard);
+        if (winning(newBoard, hu)) {
+            return {
+                score: -10
+            };
+        } else if (winning(newBoard, a)) {
+            return {
+                score: 10
+            };
+        } else if (array.length === 0) {
+            return {
+                score: 0
+            };
+        }
+
+        var moves = [];
+        for (var i = 0; i < array.length; i++) {
+            var move = {};
+            move.index = newBoard[array[i]];
+            newBoard[array[i]] = player;
+
+            if (player == a) {
+                var g = minimax(newBoard, hu);
+                move.score = g.score;
+            } else {
+                var g = minimax(newBoard, a);
+                move.score = g.score;
+            }
+            newBoard[array[i]] = move.index;
+            moves.push(move);
+        }
+
+        var bestMove;
+        if (player === a) {
+            var bestScore = -99999;
+            for (var i = 0; i < moves.length; i++) {
+                if (moves[i].score > bestScore) {
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        } else {
+            var bestScore = 99999;
+            for (var i = 0; i < moves.length; i++) {
+                if (moves[i].score < bestScore) {
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        }
+        return moves[bestMove];
+    }
+
+//available spots
+    function avail(newBoard) {
+        return newBoard.filter(s => s != "H" && s != "A");
+    }
+
+// winning combinations
